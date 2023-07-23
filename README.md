063305734176e241e33d707c61f8325e2370e3da
Michael Kondrashin <mkondrashin@gmail.com>
2023-07-23 14:35:54 +0300

    add README

---
+++ README.md
@@ -0,0 +1,49 @@
+# Enum - autmatically generate enums for Go
+
+Enum prvide ability to create enumirated values with unmarshaling from JSON and YAML. 
+
+## Example
+
+Command 
+```commandline
+enum -package main -type TrafficLight -values=Green,Yellow,Red
+```
+will generate following code:
+```golang
+type TrafficLight int
+
+const (
+    TrafficLightGreen TrafficLight = iota
+    TrafficLightYellow
+    TrafficLightRed
+)
+
+// String - return string representation for TrafficLight value
+func (v TrafficLight)String() string {
+   ...
+}
+
+// UnmarshalJSON implements the Unmarshaler interface of the json package for TrafficLight.
+func (s *TrafficLight) UnmarshalJSON(data []byte) error {
+    ...
+}
+
+// UnmarshalYAML implements the Unmarshaler interface of the yaml.v3 package for TrafficLight.
+func (s *TrafficLight) UnmarshalYAML(unmarshal func(interface{}) error) error {
+    ...
+}
+```
+
+## Installation
+```commandline
+go get github.com/mpkondrashin/enum
+```
+
+## Using go generate
+
+Add following line to your golang code:
+```golang
+//enum -package=<name of the package> -type=<name of the enum type> -values=<coma separated list of the values>
+```
+Then run following command:
+go generate
--- main.go
+++ main.go
@@ -3,49 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
 
 var (
-	typeName    = flag.String("type", "", "output file name; default srcdir/<type>_string.go; must be set")
+	typeName    = flag.String("type", "", "name of the enum type; it will be alias to int")
 	valuesList  = flag.String("values", "", "comma-separated list of values names; must be set")
 	packageName = flag.String("package", "", "package name")
+	noPrefix    = flag.Bool("noprefix", false, "do not add type name as prefix to values")
 )
 
-// Usage is a replacement usage function for the flags package.
+const usagePrefix = "Usage of enum:\n" +
+	"\tAdd following line to your golang code:\n" +
+	"\t//enum -package=<name of the package> -type=<name of the enum type> -values=<coma separated list of the values>\n" +
+	"\tThen run following command:\n" +
+	"\tgo generate\n" +
+	"\tGenerated code will be placed into the file enum_<type name>.go\n" +
+	"\tFor more details: https://github.com/mpkondrashin/enum\n" +
+	"Flags:\n"
+
 func Usage() {
-	fmt.Fprintf(os.Stderr, "Usage of enum:\n")
-	fmt.Fprintf(os.Stderr, "\tenum [flags] -type T [directory]\n")
-	fmt.Fprintf(os.Stderr, "\tenum [flags] -type T files... # Must be a single package\n")
-	fmt.Fprintf(os.Stderr, "For more information, see:\n")
-	fmt.Fprintf(os.Stderr, "\thttps://;ekfnq;efjnq;efpkg.go.dev/golang.org/x/tools/cmd/stringer\n")
-	fmt.Fprintf(os.Stderr, "Flags:\n")
+	fmt.Fprintf(os.Stderr, usagePrefix)
 	flag.PrintDefaults()
 }
 
 func main() {
-	log.SetFlags(0)
-	log.SetPrefix("enum: ")
+	//	log.SetFlags(0)
+	//	log.SetPrefix("enum: ")
 	flag.Usage = Usage
 	flag.Parse()
-	if len(*packageName) == 0 {
-		flag.Usage()
-		os.Exit(2)
-	}
-	if len(*typeName) == 0 {
-		flag.Usage()
-		os.Exit(2)
-	}
-	if len(*valuesList) == 0 {
+	if len(*packageName)+len(*typeName)+len(*valuesList) == 0 {
 		flag.Usage()
 		os.Exit(2)
 	}
 	values := strings.Split(*valuesList, ",")
-
-	err := Run(*packageName, *typeName, values...)
-	if err != nil {
-		panic(err)
+	if err := Run(*packageName, *typeName, *noPrefix, values...); err != nil {
+		fmt.Println(err)
 	}
 }
