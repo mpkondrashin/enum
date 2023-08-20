8c9ae867c293cc01854b06d5224ae48fab1a5c47
Michael Kondrashin <mkondrashin@gmail.com>
2023-08-20 10:32:33 +0300

    remove values

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 
 var (
 	typeName    = flag.String("type", "", "name of the enum type. It will be alias to int")
-	valuesList  = flag.String("values", "", "comma-separated list of values names")
+	namesList   = flag.String("names", "", "comma-separated list of names")
 	packageName = flag.String("package", "", "package name")
 	output      = flag.String("output", "", "output filename (default enum_<type name>.go)")
-	noPrefix    = flag.Bool("noprefix", false, "do not add type name as prefix to values")
+	noPrefix    = flag.Bool("noprefix", false, "do not add type name as prefix to names")
 )
 
 const usagePrefix = "Usage of enum:\n" +
 	"\tAdd following line to your golang code:\n" +
-	"\t//enum -package=<name of the package> -type=<name of the enum type> -values=<coma separated list of the values>\n" +
+	"\t//enum -package=<name of the package> -type=<name of the enum type> -names=<coma separated list of the names>\n" +
 	"\tThen run following command:\n" +
 	"\tgo generate\n" +
 	"\tGenerated code will be placed into the file enum_<type name>.go\n" +
@@ -34,16 +34,18 @@ func main() {
 	//	log.SetPrefix("enum: ")
 	flag.Usage = Usage
 	flag.Parse()
-	if len(*packageName)+len(*typeName)+len(*valuesList) == 0 {
+	if len(*packageName)+len(*typeName)+len(*namesList) == 0 {
 		flag.Usage()
 		os.Exit(2)
 	}
-	values := strings.Split(*valuesList, ",")
+
+	names := strings.Split(*namesList, ",")
+
 	fileName := fmt.Sprintf("enum_%s.go", strings.ToLower(*typeName))
 	if *output != "" {
 		fileName = *output
 	}
-	if err := Run(fileName, *packageName, *typeName, *noPrefix, values...); err != nil {
+	if err := Run(fileName, *packageName, *typeName, *noPrefix, names); err != nil {
 		fmt.Println(err)
 	}
 }
